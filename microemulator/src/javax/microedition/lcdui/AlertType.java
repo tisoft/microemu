8311fe5f1cd7e24a941735dd8d700485e562a522
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2001-08-16 11:51:07 +0000

    no message
    git-svn-id: http://microemu.googlecode.com/svn/trunk@9 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/src/javax/microedition/lcdui/AlertType.java
+++ microemulator/src/javax/microedition/lcdui/AlertType.java
@@ -26,18 +26,25 @@ public class AlertType
 {
 
 	public static final AlertType INFO = new AlertType();
-	
+
 	public static final AlertType WARNING = new AlertType();
-	
+
 	public static final AlertType ERROR = new AlertType();
-	
+
 	public static final AlertType ALARM = new AlertType();
-	
+
 	public static final AlertType CONFIRMATION = new AlertType();
 
-	
+
 	protected AlertType()
 	{
 	}
+
+
+  public boolean playSound(Display display)
+  {
+    // Not implemented
+    return true;
+  }
 
 }
