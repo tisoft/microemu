b5b9746355a638a97c548f64092c22e18046fbf2
tisoft <tisoft@272dc960-a678-11de-8dbb-b5444cbab91d>
2003-09-25 10:59:47 +0000

    It's working! Well, not everything, but some parts.
    git-svn-id: http://microemu.googlecode.com/svn/trunk@294 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/ext/siemensapi/src/com/siemens/mp/io/File.java
+++ microemulator/ext/siemensapi/src/com/siemens/mp/io/File.java
@@ -18,7 +18,11 @@
  */
 package com.siemens.mp.io;
 
-import javax.microedition.midlet.*;
+import javax.microedition.rms.*;
+import java.util.Vector;
+import java.util.Hashtable;
+import java.io.*;
+import com.barteo.emulator.device.DeviceFactory;
 
 /**
  *
@@ -26,42 +30,47 @@ import javax.microedition.midlet.*;
  * @version
  */
 public class File {
-    
-    public File() {
-    }
+    static Vector files=new Vector();
+    static Hashtable data=new Hashtable();
     
     public int close(int fileDescriptor) {
-        System.out.println("public int close(int fileDescriptor)");
-        return 0;
+        //System.out.println("public int close(int fileDescriptor)"+files.elementAt(fileDescriptor));
+        files.setElementAt(null, fileDescriptor);
+        return 1;
     }
     
     public static int copy(java.lang.String source, java.lang.String dest) {
         System.out.println("public static int copy(java.lang.String source, java.lang.String dest)");
-        return 0;
+        return 1;
     }
     
     public static int debugWrite(java.lang.String fileName, java.lang.String infoString) {
         System.out.println("public static int debugWrite(java.lang.String fileName, java.lang.String infoString)");
-        return 0;
+        return 1;
     }
     
     public static int delete(java.lang.String fileName) {
-        System.out.println("public static int delete(java.lang.String fileName) ");
-        return 0;
+        //System.out.println("public static int delete(java.lang.String "+fileName+") ");
+        data.remove(fileName);
+        return 1;
     }
     
     public static int exists(java.lang.String fileName) {
-        System.out.println("public static int exists(java.lang.String fileName)");
-        return 0;
+        //System.out.println("public static int exists(java.lang.String "+fileName+") "+data.containsKey(fileName));
+        if (data.containsKey(fileName.intern())||
+        DeviceFactory.getDevice().getDeviceDisplay().getEmulatorContext().getClassLoader().getResourceAsStream(fileName)!=null
+        ) return 1; else return -1;
     }
     
     public static boolean isDirectory(java.lang.String pathName) {
+        System.out.println("public static boolean isDirectory(java.lang.String pathName)");
         return false;
     }
     
     public int length(int fileDescriptor) {
-        System.out.println("public int length(int fileDescriptor)");
-        return 0;
+        //System.out.println("public int length(int fileDescriptor)");
+        return ((FileInfo)data.get(files.elementAt(fileDescriptor))).data.length;
+        
     }
     
     public static java.lang.String[] list(java.lang.String pathName) {
@@ -70,28 +79,62 @@ public class File {
     }
     
     public int open(java.lang.String fileName){
-        System.out.println("public int open(java.lang.String "+fileName+")");
-        return 0;
+        //System.out.println("public int open(java.lang.String "+fileName+")");
+        
+        
+        files.addElement(fileName.intern());
+        byte[] s=new byte[0];
+        if (!data.containsKey(fileName)) {
+            data.put(fileName.intern(), new FileInfo(s));
+            try {
+                InputStream is=DeviceFactory.getDevice().getDeviceDisplay().getEmulatorContext().getClassLoader().getResourceAsStream(fileName);
+
+                if(is!=null) {
+                    BufferedInputStream bis=new BufferedInputStream(is);
+                    
+                    int l;
+                    byte[] b=new byte[1024];
+                    while((l=bis.read(b))!=-1) {
+                        byte[] t=new byte[l+s.length];
+                        System.arraycopy(s, 0, t, 0, s.length);
+                        System.arraycopy(b, 0, t, s.length, l);
+                        s=t;
+                    }
+                    data.put(fileName.intern(), new FileInfo(s));
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return files.indexOf(fileName);
     }
     
     public int read(int fileDescriptor, byte[] buf, int offset, int numBytes) {
-        System.out.println("public int read(int fileDescriptor, byte[] buf, int offset, int numBytes)");
-        return 0;
+        //System.out.println("public int read(int fileDescriptor, byte[] buf, int offset, int numBytes)"+files.elementAt(fileDescriptor));
+        FileInfo f=((FileInfo)data.get(files.elementAt(fileDescriptor)));
+        
+        if(numBytes>(f.data.length-f.seek)) numBytes= f.data.length-f.seek;
+        
+        System.arraycopy(f.data, f.seek, buf, offset, numBytes);
+        f.seek+=numBytes;
+        return numBytes;
     }
     
     public static int rename(java.lang.String source, java.lang.String dest) {
         System.out.println("public static int rename(java.lang.String source, java.lang.String dest)");
-        return 0;
+        return 1;
     }
     
     public int seek(int fileDescriptor, int seekpos) {
-        System.out.println("public int seek(int fileDescriptor, int seekpos)");
-        return 0;
+        //System.out.println("public int seek(int fileDescriptor, int seekpos)");
+        ((FileInfo)data.get(files.elementAt(fileDescriptor))).seek=seekpos;
+        return 1;
     }
     
     public static int spaceAvailable() {
-        System.out.println("public static int spaceAvailable()");
-        return 0;
+        //System.out.println("public static int spaceAvailable()");
+        return 1000000;
     }
     
     public static void truncate(int fileDescriptor, int size) {
@@ -99,7 +142,35 @@ public class File {
     }
     
     public int write(int fileDescriptor, byte[] buf, int offset, int numBytes) {
-        System.out.println("public int write(int fileDescriptor, byte[] buf, int offset, int numBytes)");
-        return 0;
+        //System.out.println("public int write(int fileDescriptor, byte[] buf, int offset, int numBytes)"+files.elementAt(fileDescriptor));
+        FileInfo f=((FileInfo)data.get(files.elementAt(fileDescriptor)));
+        
+        if(numBytes>(f.data.length+f.seek)) {
+            byte[] t=new byte[f.data.length+numBytes];
+            System.arraycopy(f.data, 0, t, 0, f.data.length);
+            f.data=t;
+        }
+        
+        System.arraycopy(buf,offset,f.data,f.seek,numBytes);
+        f.seek+=numBytes;
+        
+        /*for (int i=0;i<f.data.length;i++)
+        {
+            System.out.print((char)f.data[i]);
+        }
+        System.out.println();*/
+        
+        return 1;
+    }
+    
+    
+    class FileInfo {
+        byte[] data;
+        int seek;
+        
+        FileInfo(byte[] data) {
+            this.data=data;
+            this.seek=0;
+        }
     }
 }
