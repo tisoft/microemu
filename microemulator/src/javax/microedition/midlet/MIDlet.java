95e4b0e4347e7da3a632a3d2e4dfaa55625cebe0
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2003-11-04 16:25:34 +0000

    added missing notifyPaused() method
    git-svn-id: http://microemu.googlecode.com/svn/trunk@315 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/src/javax/microedition/midlet/MIDlet.java
+++ microemulator/src/javax/microedition/midlet/MIDlet.java
@@ -1,101 +1,94 @@
 /*
- *  MicroEmulator
- *  Copyright (C) 2001 Bartek Teodorczyk <barteo@it.pl>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * MicroEmulator 
+ * Copyright (C) 2001 Bartek Teodorczyk <barteo@it.pl>
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
- 
+
 package javax.microedition.midlet;
 
-import javax.microedition.lcdui.*;
+import javax.microedition.lcdui.Display;
 
 import com.barteo.emulator.MIDletAccess;
 import com.barteo.emulator.MIDletBridge;
 
-
-public abstract class MIDlet
+public abstract class MIDlet 
 {
 	private boolean destroyed;
 
-	class MIDletAccessor extends MIDletAccess
+	class MIDletAccessor extends MIDletAccess 
 	{
+		public MIDletAccessor(MIDlet amidlet) 
+		{
+			super(amidlet);
+		}
 
-    public MIDletAccessor(MIDlet amidlet)
-    {
-      super(amidlet);
-    }
-    
+		public void startApp() throws MIDletStateChangeException 
+		{
+			MIDletBridge.setCurrentMIDlet(midlet);
+			getDisplayAccess().updateCommands();
+			midlet.startApp();
+		}
 
-	  public void startApp()
-  		  throws MIDletStateChangeException
-    {
-      MIDletBridge.setCurrentMIDlet(midlet);
-      getDisplayAccess().updateCommands();
-      midlet.startApp();
-    }
+		public void pauseApp() 
+		{
+			midlet.pauseApp();
+		}
 
-
-  	public void pauseApp()
-    {
-      midlet.pauseApp();
-    }
-
-
-  	public void destroyApp(boolean unconditional)
-    		throws MIDletStateChangeException
-    {
-    	if (!destroyed) {
-      	midlet.destroyApp(unconditional);
-    	}
-      getDisplayAccess().clean();
-    }
-
-  }
-
-
-	protected MIDlet()
-	{
-    MIDletBridge.setAccess(this, new MIDletAccessor(this));
-        
-    // initialize display
-    Display.getDisplay(this);
-    destroyed = false;
+		public void destroyApp(boolean unconditional) throws MIDletStateChangeException 
+		{
+			if (!destroyed) {
+				midlet.destroyApp(unconditional);
+			}
+			getDisplayAccess().clean();
+		}
 	}
 
+	
+	protected MIDlet() 
+	{
+		MIDletBridge.setAccess(this, new MIDletAccessor(this));
 
-	protected abstract void startApp()
-  		throws MIDletStateChangeException;
+		// initialize display
+		Display.getDisplay(this);
+		destroyed = false;
+	}
 
+	
+	protected abstract void startApp() throws MIDletStateChangeException;
 
 	protected abstract void pauseApp();
 
+	protected abstract void destroyApp(boolean unconditional) throws MIDletStateChangeException;
 
-	protected abstract void destroyApp(boolean unconditional)
-  		throws MIDletStateChangeException;
+	
+	public final String getAppProperty(String key) 
+	{
+		return MIDletBridge.getAppProperty(key);
+	}
 
-
-  public final String getAppProperty(String key)
-  {
-    return MIDletBridge.getAppProperty(key);
-  }
-
-
-	public final void notifyDestroyed()
+	
+	public final void notifyDestroyed() 
 	{
 		destroyed = true;
-    MIDletBridge.notifyDestroyed();
+		MIDletBridge.notifyDestroyed();
+	}
+
+	
+	public final void notifyPaused() 
+	{
 	}
 
 }
