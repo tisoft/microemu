8b30dc3fafec0818c1c4c26a3f5de0e73dfc8419
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2003-11-13 19:36:21 +0000

    code reformat
    git-svn-id: http://microemu.googlecode.com/svn/trunk@317 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/src/javax/microedition/lcdui/StringComponent.java
+++ microemulator/src/javax/microedition/lcdui/StringComponent.java
@@ -1,65 +1,63 @@
 /*
- *  MicroEmulator
- *  Copyright (C) 2001 Bartek Teodorczyk <barteo@it.pl>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * MicroEmulator 
+ * Copyright (C) 2001 Bartek Teodorczyk <barteo@it.pl>
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
- 
+
 package javax.microedition.lcdui;
 
 import com.barteo.emulator.device.DeviceFactory;
 
-
-class StringComponent
+class StringComponent 
 {
-
-  String text;
+	String text;
 	int breaks[] = new int[4];
 	boolean invertPaint = false;
 	int numOfBreaks;
 	int width;
 
+	
+	StringComponent() 
+	{
+		this(null);
+	}
 
-  StringComponent()
-  {
-    this(null);
-  }
-
-
-  StringComponent(String text)
-  {
-    this.text = text;
+	
+	StringComponent(String text) 
+	{
+		this.text = text;
 		width = DeviceFactory.getDevice().getDeviceDisplay().getWidth();
 		updateBreaks();
-  }
-
-
-	String getText()
-	{
-    return text;
 	}
 
-
-	void setText(String text)
+	
+	String getText() 
 	{
-    this.text = text;
+		return text;
+	}
+
+	
+	void setText(String text) 
+	{
+		this.text = text;
 		updateBreaks();
 	}
 
-
-	int getCharPositionX(int num)
+	
+	int getCharPositionX(int num) 
 	{
 		int i, prevIndex = 0;
 		Font f = Font.getDefaultFont();
@@ -73,8 +71,8 @@ class StringComponent
 		return f.substringWidth(text, prevIndex, num - prevIndex);
 	}
 
-
-	int getCharPositionY(int num)
+	
+	int getCharPositionY(int num) 
 	{
 		int y = 0;
 		Font f = Font.getDefaultFont();
@@ -88,15 +86,15 @@ class StringComponent
 
 		return y;
 	}
+
 	
-	
-	int getCharHeight()
+	int getCharHeight() 
 	{
 		return Font.getDefaultFont().getHeight();
 	}
 
-
-	int getHeight()
+	
+	int getHeight() 
 	{
 		int height;
 		Font f = Font.getDefaultFont();
@@ -111,7 +109,8 @@ class StringComponent
 
 		height = numOfBreaks * f.getHeight();
 
-		if (breaks[numOfBreaks - 1] == text.length() - 1 && text.charAt(text.length() - 1) == '\n') {
+		if (breaks[numOfBreaks - 1] == text.length() - 1
+			&& text.charAt(text.length() - 1) == '\n') {
 		} else {
 			height += f.getHeight();
 		}
@@ -119,8 +118,8 @@ class StringComponent
 		return height;
 	}
 
-
-	void insertBreak(int pos)
+	
+	void insertBreak(int pos) 
 	{
 		int i;
 
@@ -129,31 +128,31 @@ class StringComponent
 				break;
 			}
 		}
-    if (numOfBreaks + 1 == breaks.length) {
-      int newbreaks[] = new int[breaks.length + 4];
-      System.arraycopy(breaks, 0, newbreaks, 0, numOfBreaks);
-      breaks = newbreaks;
-    }
-    System.arraycopy(breaks, i, breaks, i + 1, numOfBreaks - i);
+		if (numOfBreaks + 1 == breaks.length) {
+			int newbreaks[] = new int[breaks.length + 4];
+			System.arraycopy(breaks, 0, newbreaks, 0, numOfBreaks);
+			breaks = newbreaks;
+		}
+		System.arraycopy(breaks, i, breaks, i + 1, numOfBreaks - i);
 		breaks[i] = pos;
 		numOfBreaks++;
 	}
 
+	
+	void invertPaint(boolean state) 
+	{
+		invertPaint = state;
+	}
 
-  void invertPaint(boolean state)
-  {
-    invertPaint = state;
-  }
-
-
-	int length()
+	
+	int length() 
 	{
 		return text.length();
 	}
 
-
-  int paint(Graphics g)
-  {
+	
+	int paint(Graphics g) 
+	{
 		if (text == null) {
 			return 0;
 		}
@@ -161,50 +160,56 @@ class StringComponent
 		int i, prevIndex, y;
 		Font f = Font.getDefaultFont();
 
-    for (i = prevIndex = y = 0; i < numOfBreaks; i++) {
-      if (invertPaint) {
-        g.setGrayScale(0);
-      } else {
-        g.setGrayScale(255);
-      }
-      g.fillRect(0, y, width, f.getHeight());
-      if (invertPaint) {
-        g.setGrayScale(255);
-      } else {
-        g.setGrayScale(0);
-      }
-      g.drawSubstring(text, prevIndex, breaks[i] - prevIndex, 0, y, 0);
+		for (i = prevIndex = y = 0; i < numOfBreaks; i++) {
+			if (invertPaint) {
+				g.setGrayScale(0);
+			} else {
+				g.setGrayScale(255);
+			}
+			g.fillRect(0, y, width, f.getHeight());
+			if (invertPaint) {
+				g.setGrayScale(255);
+			} else {
+				g.setGrayScale(0);
+			}
+			g.drawSubstring(text, prevIndex, breaks[i] - prevIndex, 0, y, 0);
 			prevIndex = breaks[i];
 			y += f.getHeight();
-    }
+		}
 		if (prevIndex != text.length()) {
-	    if (invertPaint) {
-	      g.setGrayScale(0);
-	    } else {
-	      g.setGrayScale(255);
-	    }
-	    g.fillRect(0, y, width, f.getHeight());
-	    if (invertPaint) {
-	      g.setGrayScale(255);
-	    } else {
-	      g.setGrayScale(0);
-	    }
-	    g.drawSubstring(text, prevIndex, text.length() - prevIndex, 0, y, 0);
+			if (invertPaint) {
+				g.setGrayScale(0);
+			} else {
+				g.setGrayScale(255);
+			}
+			g.fillRect(0, y, width, f.getHeight());
+			if (invertPaint) {
+				g.setGrayScale(255);
+			} else {
+				g.setGrayScale(0);
+			}
+			g.drawSubstring(
+				text,
+				prevIndex,
+				text.length() - prevIndex,
+				0,
+				y,
+				0);
 			y += f.getHeight();
 		}
 
 		return y;
-  }
+	}
 
-
-	void setWidth(int width)
+	
+	void setWidth(int width) 
 	{
 		this.width = width;
 		updateBreaks();
 	}
 
-
-	void updateBreaks()
+	
+	void updateBreaks() 
 	{
 		if (text == null) {
 			return;
