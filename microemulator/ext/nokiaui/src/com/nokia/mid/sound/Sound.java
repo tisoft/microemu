f212e8e7f2a1045b4f12238326ff728b6b89f7f8
tisoft <tisoft@272dc960-a678-11de-8dbb-b5444cbab91d>
2004-04-27 21:20:28 +0000

    *** empty log message ***
    git-svn-id: http://microemu.googlecode.com/svn/trunk@344 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/ext/nokiaui/src/com/nokia/mid/sound/Sound.java
+++ microemulator/ext/nokiaui/src/com/nokia/mid/sound/Sound.java
@@ -43,7 +43,7 @@ public class Sound
     
     public static int getConcurrentSoundCount(int type) 
     {
-        throw new java.lang.IllegalArgumentException();
+        return 0;
     }
     
     
