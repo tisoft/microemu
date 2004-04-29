6af2a869ff15af1f89472721e51fdde4b49fe707
tisoft <tisoft@272dc960-a678-11de-8dbb-b5444cbab91d>
2004-04-29 12:46:31 +0000

    initial release
    git-svn-id: http://microemu.googlecode.com/svn/trunk@345 272dc960-a678-11de-8dbb-b5444cbab91d

---
+++ microemulator/ext/siemensapi/src/com/siemens/mp/media/control/ToneControl.java
@@ -0,0 +1,38 @@
+/*
+ *  Siemens API for MicroEmulator
+ *  Copyright (C) 2003 Markus Heberling <markus@heberling.net>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.siemens.mp.media.control;
+
+import com.siemens.mp.media.Control;
+
+public interface ToneControl
+    extends Control
+{
+    public abstract void setSequence(byte abyte0[]);
+
+    public static final byte VERSION = -2;
+    public static final byte TEMPO = -3;
+    public static final byte RESOLUTION = -4;
+    public static final byte BLOCK_START = -5;
+    public static final byte BLOCK_END = -6;
+    public static final byte PLAY_BLOCK = -7;
+    public static final byte SET_VOLUME = -8;
+    public static final byte REPEAT = -9;
+    public static final byte C4 = 60;
+    public static final byte SILENCE = -1;
+}
