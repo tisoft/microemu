d5a40906bc29416c21b82fa998d8a7353ea25ea6
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2006-11-28 12:11:02 +0000

    updated fields according to specification
    git-svn-id: http://microemu.googlecode.com/svn/trunk@522 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/midp/midp2/src/javax/microedition/media/PlayerListener.java
+++ microemulator/midp/midp2/src/javax/microedition/media/PlayerListener.java
@@ -22,23 +22,23 @@ package javax.microedition.media;
 public interface PlayerListener
 {
 
-	public static final String STARTED = "STARTED";
+	public static final String STARTED = "started";
 	
-	public static final String STOPPED = "STOPPED";
+	public static final String STOPPED = "stopped";
 	
-	public static final String END_OF_MEDIA = "END_OF_MEDIA";
+	public static final String END_OF_MEDIA = "endOfMedia";
 	
-	public static final String DURATION_UPDATED = "DURATION_UPDATED";
+	public static final String DURATION_UPDATED = "durationUpdated";
 	
-	public static final String DEVICE_UNAVAILABLE = "DEVICE_UNAVAILABLE";
+	public static final String DEVICE_UNAVAILABLE = "deviceUnavailable";
 	
-	public static final String DEVICE_AVAILABLE = "DEVICE_AVAILABLE";
+	public static final String DEVICE_AVAILABLE = "deviceAvailable";
 	
-	public static final String VOLUME_CHANGED = "VOLUME_CHANGED";
+	public static final String VOLUME_CHANGED = "volumeChanged";
 	
-	public static final String ERROR = "ERROR";
+	public static final String ERROR = "error";
 	
-	public static final String CLOSED = "CLOSED";
+	public static final String CLOSED = "closed";
 	
 	public void playerUpdate(Player player, String event, Object eventData);
 	
