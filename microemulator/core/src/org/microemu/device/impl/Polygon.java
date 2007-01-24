3bd31e24ba8c4c883e9f6e74da52314fd01a5894
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-01-24 00:50:13 +0000

    Fixed Polygon.contains(x,y)
    git-svn-id: http://microemu.googlecode.com/svn/trunk@607 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/core/src/org/microemu/device/impl/Polygon.java
+++ microemulator/core/src/org/microemu/device/impl/Polygon.java
@@ -23,6 +23,7 @@ package org.microemu.device.impl;
  * This class is intended for storing georaphical polygons such as boundary data
  * for wards, coastlines etc.<p>
  *
+ * Extracted from uk.ac.leeds.ccg.geotools.GeoPolygon
  *
  * @author James Macgill
  * @author Mathieu Van Loon - getPointsAsArrayX, getPointsAsArrayY, dropVector
@@ -44,7 +45,7 @@ public class Polygon extends Shape {
 	 * Array of points.
 	 */
 	public int[] ypoints;
-	
+
 	private Rectangle bounds = new Rectangle();
 
 	/**
@@ -71,7 +72,7 @@ public class Polygon extends Shape {
 		//Add a try here to catch failed array copy
 		System.arraycopy(xpoints, 0, this.xpoints, 0, npoints);
 		System.arraycopy(ypoints, 0, this.ypoints, 0, npoints);
-		
+
 		for (int i = 0; i < npoints; i++) {
 			bounds.add(xpoints[i], ypoints[i]);
 		}
@@ -112,7 +113,7 @@ public class Polygon extends Shape {
 		npoints++;
 		xpoints[npoints - 1] = x;//-1 to account for 0 array indexing
 		ypoints[npoints - 1] = y;
-		
+
 		bounds.add(x, y);
 	}
 
@@ -121,10 +122,12 @@ public class Polygon extends Shape {
 	}
 
 	/**
-	 * Tests to see if a point is contained by this polygon.
-	 * @param p The GeoPoint to test.
-	 * @return boolean True if point is contained by this polygon.
-	 */
+     * Tests to see if a point is contained by this polygon.
+     * 
+     * @param p
+     *            The GeoPoint to test.
+     * @return boolean True if point is contained by this polygon.
+     */
 	public boolean contains(int x, int y) {
 
 		/*if(!getMultiPartBounds().contains(p)){
@@ -133,53 +136,61 @@ public class Polygon extends Shape {
 
 		if (getBounds().contains(x, y)) {
 
-			/* Start andys code */
-			int number_of_lines_crossed = 0; // holds lines intersecting with 
-			number_of_lines_crossed = 0; // set lines crossed = 0 for each polygon
-			// lets us know which part start we're looking for	   
-			for (int i = 0; i < npoints - 1; i++) {
-				//GeoPoint A = new GeoPoint();
-				//GeoPoint B = new GeoPoint();        
-				//A.y = ypoints[i];                                       // get y-coordinate
-				//A.x = xpoints[i];                                       // get x-coordinate 
-				//B.y = ypoints[i+1];                                     // get next y-coordinate
-				//B.x = xpoints[i+1];                                     // get next x-coordinate
-
-				if (y != ypoints[i + 1]
-						&& (y <= Math.max(ypoints[i], ypoints[i + 1]))
-						&& (y >= Math.min(ypoints[i], ypoints[i + 1]))
-						&& ((xpoints[i] >= x) || (xpoints[i + 1] >= x))) { // if polygon contains a suitable value
-					if (((xpoints[i] >= x) && (xpoints[i + 1] >= x))) {
-						number_of_lines_crossed++;//simple case
-					} else {
-						double gradient; //   calc. gradient
-						if (xpoints[i] > xpoints[i + 1])
-							gradient = ((xpoints[i] - xpoints[i + 1]) / (ypoints[i] - ypoints[i + 1]));
-						else
-							gradient = ((xpoints[i + 1] - xpoints[i]) / (ypoints[i + 1] - ypoints[i]));
-						double x_intersection_axis = (xpoints[i] - (gradient * ypoints[i])); // calc. intersect with x-axis
-						double x_intersection_line = (gradient * y)
-								+ x_intersection_axis; // calc. intersect with y=const
-						//  line extending from location 
-						if ((x_intersection_line <= Math.max(xpoints[i],
-								xpoints[i + 1]))
-								&& // check intersect inside polygon 
-								(x_intersection_line >= Math.min(xpoints[i],
-										xpoints[i + 1]))
-								&& (x_intersection_line >= x)) {
-							number_of_lines_crossed++; // increment line counter
-						} // end check for inside polygon
-					}
-				} // end of if polygon points suitable
-			} // end of run through points in polygon
-
-			if ((number_of_lines_crossed != 0) && // if number of polygon lines crossed
-					(((number_of_lines_crossed % 2) == 1))) { //   by a line in one direction from the
-				return true; //   initial location is odd, the location
-
-				//   lies in that polygon
-
-			} // end of run through polygons
+//          Tested and work just fine
+			boolean c = false;
+			for (int i = 0, j = npoints - 1; i < npoints; j = i++) {
+				if ((((ypoints[i] <= y) && (y < ypoints[j])) || ((ypoints[j] <= y) && (y < ypoints[i])))
+						&& (x < ((double)(xpoints[j] - xpoints[i]) * (y - ypoints[i])) / (ypoints[j] - ypoints[i]) + xpoints[i])) {
+					c = !c;
+				}
+			}
+			return c;
+			
+//           This code does not work as expected even after fixes for double
+//
+//			/* Start andys code */
+//			int number_of_lines_crossed = 0; // holds lines intersecting with 
+//			number_of_lines_crossed = 0; // set lines crossed = 0 for each polygon
+//			// lets us know which part start we're looking for	   
+//			for (int i = 0; i < npoints - 1; i++) {
+//				//GeoPoint A = new GeoPoint();
+//				//GeoPoint B = new GeoPoint();        
+//				//A.y = ypoints[i];                                       // get y-coordinate
+//				//A.x = xpoints[i];                                       // get x-coordinate 
+//				//B.y = ypoints[i+1];                                     // get next y-coordinate
+//				//B.x = xpoints[i+1];                                     // get next x-coordinate
+//
+//				if (y != ypoints[i + 1] && (y <= Math.max(ypoints[i], ypoints[i + 1]))
+//						&& (y >= Math.min(ypoints[i], ypoints[i + 1])) && ((xpoints[i] >= x) || (xpoints[i + 1] >= x))) { // if polygon contains a suitable value
+//					if (((xpoints[i] >= x) && (xpoints[i + 1] >= x))) {
+//						number_of_lines_crossed++;//simple case
+//					} else {
+//						double gradient; //   calc. gradient
+//						if (xpoints[i] > xpoints[i + 1]) {
+//							gradient = ((double) (xpoints[i] - xpoints[i + 1]) / (double) (ypoints[i] - ypoints[i + 1]));
+//						} else {
+//							gradient = ((double) (xpoints[i + 1] - xpoints[i]) / (double) (ypoints[i + 1] - ypoints[i]));
+//						}
+//						double x_intersection_axis = (xpoints[i] - (gradient * ypoints[i])); // calc. intersect with x-axis
+//						double x_intersection_line = (gradient * y) + x_intersection_axis; // calc. intersect with y=const
+//						//  line extending from location 
+//						if ((x_intersection_line <= Math.max(xpoints[i], xpoints[i + 1]))
+//								&& // check intersect inside polygon 
+//								(x_intersection_line >= Math.min(xpoints[i], xpoints[i + 1]))
+//								&& (x_intersection_line >= x)) {
+//							number_of_lines_crossed++; // increment line counter
+//						} // end check for inside polygon
+//					}
+//				} // end of if polygon points suitable
+//			} // end of run through points in polygon
+//
+//			if ((number_of_lines_crossed != 0) && // if number of polygon lines crossed
+//					(((number_of_lines_crossed % 2) == 1))) { //   by a line in one direction from the
+//				return true; //   initial location is odd, the location
+//
+//				//   lies in that polygon
+//
+//			} // end of run through polygons
 
 		}
 		return false; // return stuffing if things get this far
