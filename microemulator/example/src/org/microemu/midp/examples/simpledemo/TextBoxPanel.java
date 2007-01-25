121ada4acd569502a47074c330ad2495dfdaa3c5
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-01-25 06:51:39 +0000

    Add OK to exit from text box
    git-svn-id: http://microemu.googlecode.com/svn/trunk@614 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/example/src/org/microemu/midp/examples/simpledemo/TextBoxPanel.java
+++ microemulator/example/src/org/microemu/midp/examples/simpledemo/TextBoxPanel.java
@@ -16,65 +16,58 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
+
 package org.microemu.midp.examples.simpledemo;
 
 import javax.microedition.lcdui.*;
 
+public class TextBoxPanel extends List implements ScreenPanel, CommandListener {
 
-public class TextBoxPanel extends List implements ScreenPanel, CommandListener
-{
+	static String NAME = "TextBox";
 
-  static String NAME = "TextBox";
+	TextBox textBoxes[] = { new TextBox("Any character", null, 128, TextField.ANY),
+			new TextBox("Email", null, 128, TextField.EMAILADDR), 
+			new TextBox("Numeric", null, 128, TextField.NUMERIC),
+			new TextBox("Phone number", null, 128, TextField.PHONENUMBER),
+			new TextBox("URL", null, 128, TextField.URL), 
+			new TextBox("Password", null, 128, TextField.PASSWORD), };
 
-  TextBox textBoxes[] = {
-      new TextBox("Any character", null, 128, TextField.ANY),
-      new TextBox("Email", null, 128, TextField.EMAILADDR),
-      new TextBox("Numeric", null, 128, TextField.NUMERIC),
-      new TextBox("Phone number", null, 128, TextField.PHONENUMBER),
-      new TextBox("URL", null, 128, TextField.URL),
-      new TextBox("Password", null, 128, TextField.PASSWORD),
-  };  
-  
-  Command backCommand = new Command("Back", Command.BACK, 1);
-  
-  
-  public TextBoxPanel()
-  {
-    super(NAME, List.IMPLICIT);
-    
-    for (int i = 0; i < textBoxes.length; i++) {
-      textBoxes[i].addCommand(backCommand);
-      textBoxes[i].setCommandListener(this);      
-      append(textBoxes[i].getTitle(), null);
-    }
-    
-    addCommand(backCommand);
-    setCommandListener(this);
-  }
-    
-    
-  public String getName()
-  {
-    return NAME;
-  }
-  
-  
-  public void commandAction(Command c, Displayable d)
-  {
-    if (d == this) {
-      if (c == List.SELECT_COMMAND) {
-        Display.getDisplay(SimpleDemo.getInstance()).setCurrent(textBoxes[getSelectedIndex()]);
-      } else if (c == backCommand) {
-        Display.getDisplay(SimpleDemo.getInstance()).setCurrent(SimpleDemo.getInstance().menuList);
-      }
-    } else if (c == backCommand) {
-      for (int i = 0; i < textBoxes.length; i++) {
-        if (d == textBoxes[i]) {
-          Display.getDisplay(SimpleDemo.getInstance()).setCurrent(this);
-        }
-      }
-    }
-  }
+	Command backCommand = new Command("Back", Command.BACK, 1);
+
+	Command okCommand = new Command("Ok", Command.OK, 2);
+
+	public TextBoxPanel() {
+		super(NAME, List.IMPLICIT);
+
+		for (int i = 0; i < textBoxes.length; i++) {
+			textBoxes[i].addCommand(backCommand);
+			textBoxes[i].addCommand(okCommand);
+			textBoxes[i].setCommandListener(this);
+			append(textBoxes[i].getTitle(), null);
+		}
+
+		addCommand(backCommand);
+		setCommandListener(this);
+	}
+
+	public String getName() {
+		return NAME;
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		if (d == this) {
+			if (c == List.SELECT_COMMAND) {
+					Display.getDisplay(SimpleDemo.getInstance()).setCurrent(textBoxes[getSelectedIndex()]);
+			} else if (c == backCommand) {
+				Display.getDisplay(SimpleDemo.getInstance()).setCurrent(SimpleDemo.getInstance().menuList);
+			}
+		} else if ((c == backCommand) || (c == okCommand)) {
+			for (int i = 0; i < textBoxes.length; i++) {
+				if (d == textBoxes[i]) {
+					Display.getDisplay(SimpleDemo.getInstance()).setCurrent(this);
+				}
+			}
+		}
+	}
 
 }
