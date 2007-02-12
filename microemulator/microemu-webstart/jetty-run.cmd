2289de71ba15818461ddbd5ba5b7cf635d507514
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-02-12 23:41:47 +0000

    Show where to go for users
    git-svn-id: http://microemu.googlecode.com/svn/trunk@868 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-webstart/jetty-run.cmd
+++ microemulator/microemu-webstart/jetty-run.cmd
@@ -1,8 +1,9 @@
 @echo off
 rem @version $Revision$ ($Author$)  $Date$
-title Jetty:microemu-webstart
+title *Jetty:microemu-webstart
 
 call mvn -P debug webstart:jnlp
+echo Go to http://localhost:8080/microemu-webstart/
 call mvn %* jetty:run
 
 title Jetty:microemu-webstart - ended
