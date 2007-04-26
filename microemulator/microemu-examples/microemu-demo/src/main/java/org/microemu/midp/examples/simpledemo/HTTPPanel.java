6080007c9be7dbb5338ff0768be6808f67d95ab0
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-04-26 03:35:41 +0000

    assembly-all-sources, prepare for release
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1176 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-examples/microemu-demo/src/main/java/org/microemu/midp/examples/simpledemo/HTTPPanel.java
+++ microemulator/microemu-examples/microemu-demo/src/main/java/org/microemu/midp/examples/simpledemo/HTTPPanel.java
@@ -43,8 +43,9 @@ public class HTTPPanel extends BaseExamplesForm {
 
 	static final Command goCommand = new Command("Go", Command.OK, 1);
 	
+	// See file src/site/resources/test/index.php
 	static final String demoLocation = "http://pyx4j.com/test/";
-	//"http://www.microemu.org/test.php";
+	//"http://www.microemu.org/test/";
 	
 	TextField url;
 	
