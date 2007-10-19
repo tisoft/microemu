1141528c0b619a39e019e8c8d8af3dd1c9599eeb
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-10-19 17:55:32 +0000

    fixed when instrumentMIDletClasses=false
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1427 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase/src/main/java/org/microemu/app/classloader/MIDletClassLoader.java
+++ microemulator/microemu-javase/src/main/java/org/microemu/app/classloader/MIDletClassLoader.java
@@ -40,50 +40,49 @@ import org.microemu.app.util.IOUtils;
 import org.microemu.log.Logger;
 
 /**
- * Main features of this class loader
- * Security aware  - enables load and run app in Webstart.
- * Proper class loading order. MIDlet classes loaded first then system and MicroEmulator classes
- * Proper resource loading order. MIDlet resources only can be loaded.
- * MIDlet Bytecode preprocessing/instrumentation  
+ * Main features of this class loader Security aware - enables load and run app
+ * in Webstart. Proper class loading order. MIDlet classes loaded first then
+ * system and MicroEmulator classes Proper resource loading order. MIDlet
+ * resources only can be loaded. MIDlet Bytecode preprocessing/instrumentation
  * 
  * @author vlads
- *
+ * 
  */
 public class MIDletClassLoader extends URLClassLoader {
 
-	//TODO make this configurable
-	
+	// TODO make this configurable
+
 	public static boolean instrumentMIDletClasses = true;
-	
+
 	public static boolean traceClassLoading = false;
-	
+
 	public static boolean traceSystemClassLoading = false;
-	
-	public static boolean enhanceCatchBlock = false;  
-	
+
+	public static boolean enhanceCatchBlock = false;
+
 	private final static boolean debug = false;
 
 	private boolean delegatingToParent = false;
-	
+
 	private InstrumentationConfig config;
-	
+
 	private Set noPreporcessingNames;
-	
+
 	/* The context to be used when loading classes and resources */
-    private AccessControlContext acc;
-    
-    private static class LoadClassByParentException extends ClassNotFoundException {
-		
-    	public LoadClassByParentException(String name) {
+	private AccessControlContext acc;
+
+	private static class LoadClassByParentException extends ClassNotFoundException {
+
+		public LoadClassByParentException(String name) {
 			super(name);
 		}
 
 		private static final long serialVersionUID = 1L;
-    	
-    }
-    
+
+	}
+
 	public MIDletClassLoader(ClassLoader parent) {
-		super(new URL[]{}, parent);
+		super(new URL[] {}, parent);
 		noPreporcessingNames = new HashSet();
 		acc = AccessController.getContext();
 		config = new InstrumentationConfig();
@@ -91,30 +90,32 @@ public class MIDletClassLoader extends URLClassLoader {
 		config.setEnhanceThreadCreation(true);
 	}
 
-//	public MIDletClassLoader(URL[] urls, ClassLoader parent) {
-//		super(urls, parent);
-//		noPreporcessingNames = new HashSet();
-//	}
-	
+	// public MIDletClassLoader(URL[] urls, ClassLoader parent) {
+	// super(urls, parent);
+	// noPreporcessingNames = new HashSet();
+	// }
+
 	public void configure(MIDletClassLoaderConfig clConfig) throws MalformedURLException {
 		for (Iterator iter = clConfig.appclasspath.iterator(); iter.hasNext();) {
-			String path = (String)iter.next();
+			String path = (String) iter.next();
 			StringTokenizer st = new StringTokenizer(path, File.pathSeparator);
 			while (st.hasMoreTokens()) {
 				this.addURL(new URL(IOUtils.getCanonicalFileClassLoaderURL(new File(st.nextToken()))));
 			}
-		}	
+		}
 		for (Iterator iter = clConfig.appclasses.iterator(); iter.hasNext();) {
 			this.addClassURL((String) iter.next());
 		}
 		this.delegatingToParent = (clConfig.delegationType == MIDletClassLoaderConfig.DELEGATION_DELEGATING);
 	}
-    /**
-     * Appends the Class Location URL to the list of URLs to search for
-     * classes and resources.
-     *
-     * @param Class Name
-     */
+
+	/**
+	 * Appends the Class Location URL to the list of URLs to search for classes
+	 * and resources.
+	 * 
+	 * @param Class
+	 *            Name
+	 */
 	public void addClassURL(String className) throws MalformedURLException {
 		String resource = getClassResourceName(className);
 		URL url = getParent().getResource(resource);
@@ -129,8 +130,8 @@ public class MIDletClassLoader extends URLClassLoader {
 			Logger.debug("addClassURL ", path);
 		}
 		addURL(new URL(path.substring(0, path.length() - resource.length())));
-    }
-	
+	}
+
 	static URL getClassURL(ClassLoader parent, String className) throws MalformedURLException {
 		String resource = getClassResourceName(className);
 		URL url = parent.getResource(resource);
@@ -139,41 +140,56 @@ public class MIDletClassLoader extends URLClassLoader {
 		}
 		String path = url.toExternalForm();
 		return new URL(path.substring(0, path.length() - resource.length()));
-    }
-	
+	}
+
 	public void addURL(URL url) {
 		if (debug) {
 			Logger.debug("addURL ", url.toString());
 		}
 		super.addURL(url);
 	}
-    
+
 	/**
 	 * Loads the class with the specified <a href="#name">binary name</a>.
 	 * 
-	 * <p> Search order is reverse to standard implemenation</p>
+	 * <p>
+	 * Search order is reverse to standard implemenation
+	 * </p>
 	 * 
-	 * This implementation of this method searches for classes in the
-	 * following order:
-	 *
-	 * <p><ol>
-	 *
-	 *   <li><p> Invoke {@link #findLoadedClass(String)} to check if the class
-	 *   has already been loaded.  </p></li>
-	 *
-	 *   <li><p> Invoke the {@link #findClass(String)} method to find the
-	 *   class in this class loader URLs.  </p></li>
-	 *
-	 *   <li><p> Invoke the {@link #loadClass(String) <tt>loadClass</tt>} method
-	 *   on the parent class loader.  If the parent is <tt>null</tt> the class
-	 *   loader built-in to the virtual machine is used, instead.  </p></li>
-	 *
+	 * This implementation of this method searches for classes in the following
+	 * order:
+	 * 
+	 * <p>
+	 * <ol>
+	 * 
+	 * <li>
+	 * <p>
+	 * Invoke {@link #findLoadedClass(String)} to check if the class has already
+	 * been loaded.
+	 * </p>
+	 * </li>
+	 * 
+	 * <li>
+	 * <p>
+	 * Invoke the {@link #findClass(String)} method to find the class in this
+	 * class loader URLs.
+	 * </p>
+	 * </li>
+	 * 
+	 * <li>
+	 * <p>
+	 * Invoke the {@link #loadClass(String) <tt>loadClass</tt>} method on the
+	 * parent class loader. If the parent is <tt>null</tt> the class loader
+	 * built-in to the virtual machine is used, instead.
+	 * </p>
+	 * </li>
+	 * 
 	 * </ol>
-	 *
+	 * 
 	 */
 	protected synchronized Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
 		if (debug) {
-			Logger.debug("loadClass",  name);
+			Logger.debug("loadClass", name);
 		}
 		// First, check if the class has already been loaded
 		Class result = findLoadedClass(name);
@@ -184,7 +200,7 @@ public class MIDletClassLoader extends URLClassLoader {
 					Logger.debug("loadClass not found", name);
 				}
 			} catch (ClassNotFoundException e) {
-				
+
 				if ((e instanceof LoadClassByParentException) || this.delegatingToParent) {
 					if (traceSystemClassLoading) {
 						Logger.info("Load system class", name);
@@ -203,36 +219,42 @@ public class MIDletClassLoader extends URLClassLoader {
 		}
 		return result;
 	}
-	
+
 	/**
-     * Finds the resource with the given name.  A resource is some data
-     * (images, audio, text, etc) that can be accessed by class code in a way
-     * that is independent of the location of the code.
-     *
-     * <p> The name of a resource is a '<tt>/</tt>'-separated path name that
-     * identifies the resource.
-     *
-     * <p> Search order is reverse to standard implemenation</p>
-     * 
-     * <p> This method will first use {@link #findResource(String)} to find the resource. 
-     * That failing, this method will NOT invoke the parent class loader if delegatingToParent=false. </p>
-     *
-     * @param  name
-     *         The resource name
-     *
-     * @return  A <tt>URL</tt> object for reading the resource, or
-     *          <tt>null</tt> if the resource could not be found or the invoker
-     *          doesn't have adequate  privileges to get the resource.
-     *
-     */
-    
+	 * Finds the resource with the given name. A resource is some data (images,
+	 * audio, text, etc) that can be accessed by class code in a way that is
+	 * independent of the location of the code.
+	 * 
+	 * <p>
+	 * The name of a resource is a '<tt>/</tt>'-separated path name that
+	 * identifies the resource.
+	 * 
+	 * <p>
+	 * Search order is reverse to standard implemenation
+	 * </p>
+	 * 
+	 * <p>
+	 * This method will first use {@link #findResource(String)} to find the
+	 * resource. That failing, this method will NOT invoke the parent class
+	 * loader if delegatingToParent=false.
+	 * </p>
+	 * 
+	 * @param name
+	 *            The resource name
+	 * 
+	 * @return A <tt>URL</tt> object for reading the resource, or
+	 *         <tt>null</tt> if the resource could not be found or the invoker
+	 *         doesn't have adequate privileges to get the resource.
+	 * 
+	 */
+
 	public URL getResource(final String name) {
 		try {
 			return (URL) AccessController.doPrivileged(new PrivilegedExceptionAction() {
 				public Object run() {
 					URL url = findResource(name);
 					if (delegatingToParent && (getParent() != null)) {
-					    url = getParent().getResource(name);
+						url = getParent().getResource(name);
 					}
 					return url;
 				}
@@ -268,13 +290,16 @@ public class MIDletClassLoader extends URLClassLoader {
 		}
 
 	}
-	
+
 	public boolean classLoadByParent(String className) {
 		/* This java standard */
 		if (className.startsWith("java.")) {
 			return true;
 		}
-		/* This is required when Class.forName().newInstance() used to create instances with inheritance*/
+		/*
+		 * This is required when Class.forName().newInstance() used to create
+		 * instances with inheritance
+		 */
 		if (className.startsWith("sun.reflect.")) {
 			return true;
 		}
@@ -290,15 +315,16 @@ public class MIDletClassLoader extends URLClassLoader {
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Special case for classes injected to MIDlet 
+	 * Special case for classes injected to MIDlet
+	 * 
 	 * @param klass
 	 */
 	public void disableClassPreporcessing(Class klass) {
 		disableClassPreporcessing(klass.getName());
 	}
-	
+
 	public void disableClassPreporcessing(String className) {
 		noPreporcessingNames.add(className);
 	}
@@ -327,7 +353,7 @@ public class MIDletClassLoader extends URLClassLoader {
 			}
 			throw new ClassNotFoundException(name, e.getCause());
 		}
-		
+
 		if (is == null) {
 			if (debug) {
 				Logger.debug("Unable to find resource for class", name);
@@ -335,37 +361,43 @@ public class MIDletClassLoader extends URLClassLoader {
 			throw new ClassNotFoundException(name);
 		}
 		byte[] byteCode;
+		int byteCodeLength;
 		try {
 			if (traceClassLoading) {
 				Logger.info("Load MIDlet class", name);
 			}
 			if (instrumentMIDletClasses) {
 				byteCode = ClassPreprocessor.instrument(is, config);
+				byteCodeLength = byteCode.length;
 			} else {
-				int chunkSize = 1024;
-				//  No class or data object must be bigger than 16 Kilobyte
-				int maxClassSizeSize = 1024 * 16;
-				byte[] readData = new byte[chunkSize];
-				int readOffset = 0;
+				final int chunkSize = 1024 * 2;
+				// No class or data object must be bigger than 16 Kilobyte
+				final int maxClassSizeSize = 1024 * 16;
+				byteCode = new byte[chunkSize];
+				byteCodeLength = 0;
 				do {
 					int retrived;
 					try {
-						retrived = is.read(readData, readOffset, chunkSize);
+						retrived = is.read(byteCode, byteCodeLength, byteCode.length - byteCodeLength);
 					} catch (IOException e) {
 						throw new ClassNotFoundException(name, e);
 					}
 					if (retrived == -1) {
 						break;
 					}
-					if (readData.length + chunkSize > maxClassSizeSize) {
-						throw new ClassNotFoundException(name, new ClassFormatError("Class object is bigger than 16 Kilobyte"));
+					if (byteCode.length + chunkSize > maxClassSizeSize) {
+						throw new ClassNotFoundException(name, new ClassFormatError(
+								"Class object is bigger than 16 Kilobyte"));
 					}
-					readOffset += chunkSize;
-					byte[] newData = new byte[readData.length + chunkSize];
-					System.arraycopy(readData, 0, newData, 0, readData.length);
-					readData = newData;
+					byteCodeLength += retrived;
+					if (byteCode.length == byteCodeLength) {
+						byte[] newData = new byte[byteCode.length + chunkSize];
+						System.arraycopy(byteCode, 0, newData, 0, byteCode.length);
+						byteCode = newData;
+					} else if (byteCode.length < byteCodeLength) {
+						throw new ClassNotFoundException(name, new ClassFormatError("Internal read error"));
+					}
 				} while (true);
-				byteCode = readData;
 			}
 		} finally {
 			try {
@@ -376,6 +408,6 @@ public class MIDletClassLoader extends URLClassLoader {
 		if ((debug) && (instrumentMIDletClasses)) {
 			Logger.debug("instrumented ", name);
 		}
-		return defineClass(name, byteCode, 0, byteCode.length);
+		return defineClass(name, byteCode, 0, byteCodeLength);
 	}
 }
