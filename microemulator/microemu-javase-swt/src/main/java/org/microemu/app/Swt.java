a2f791c7fe812be6da077fc3c2726c04b9f9a073
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2007-11-01 11:41:08 +0000

    Rearranged startup code
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1458 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase-swt/src/main/java/org/microemu/app/Swt.java
+++ microemulator/microemu-javase-swt/src/main/java/org/microemu/app/Swt.java
@@ -16,7 +16,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
+
 package org.microemu.app;
 
 import java.io.File;
@@ -25,6 +25,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
@@ -64,52 +65,48 @@ import org.microemu.device.swt.SwtDeviceDisplay;
 import org.microemu.device.swt.SwtFontManager;
 import org.microemu.device.swt.SwtInputMethod;
 
-public class Swt extends Common
-{
+public class Swt extends Common {
 	public static Shell shell;
 
 	protected static SwtDeviceComponent devicePanel;
 
 	protected MenuItem menuOpenJADFile;
+
 	protected MenuItem menuOpenJADURL;
 
 	private SwtSelectDeviceDialog selectDeviceDialog;
+
 	private FileDialog fileDialog = null;
+
 	private MenuItem menuSelectDevice;
-	    
+
 	private DeviceEntry deviceEntry;
 
 	private Label statusBar;
-  
-	private KeyListener keyListener = new KeyListener()
-	{    
-		public void keyTyped(KeyEvent e)
-		{
+
+	private KeyListener keyListener = new KeyListener() {
+		public void keyTyped(KeyEvent e) {
 		}
-    
-		public void keyPressed(KeyEvent e)
-		{
-//			devicePanel.keyPressed(e);
+
+		public void keyPressed(KeyEvent e) {
+			// devicePanel.keyPressed(e);
 		}
-    
-		public void keyReleased(KeyEvent e)
-		{
-//			devicePanel.keyReleased(e);
-		}    
+
+		public void keyReleased(KeyEvent e) {
+			// devicePanel.keyReleased(e);
+		}
 	};
-   
-	protected Listener menuOpenJADFileListener = new Listener()
-	{
-		public void handleEvent(Event ev)
-		{
+
+	protected Listener menuOpenJADFileListener = new Listener() {
+		public void handleEvent(Event ev) {
 			if (fileDialog == null) {
 				fileDialog = new FileDialog(shell, SWT.OPEN);
 				fileDialog.setText("Open JAD File...");
-				fileDialog.setFilterNames(new String[] {"JAD files"});
-				fileDialog.setFilterExtensions(new String[] {"*.jad"});
+				fileDialog.setFilterNames(new String[] { "JAD files" });
+				fileDialog.setFilterExtensions(new String[] { "*.jad" });
 				fileDialog.setFilterPath(Config.getRecentDirectory("recentJadDirectory"));
 			}
-      
+
 			fileDialog.open();
 
 			if (fileDialog.getFileName().length() > 0) {
@@ -123,13 +120,11 @@ public class Swt extends Common
 				String url = IOUtils.getCanonicalFileURL(selectedFile);
 				Common.openJadUrlSafe(url);
 			}
-		} 
+		}
 	};
-  
-	protected Listener menuOpenJADURLListener = new Listener()
-	{
-		public void handleEvent(Event ev)
-		{
+
+	protected Listener menuOpenJADURLListener = new Listener() {
+		public void handleEvent(Event ev) {
 			// TODO change to JadUrlPanel
 			SwtInputDialog inputDialog = new SwtInputDialog(shell, "Open...", "Enter JAD URL:");
 			if (inputDialog.open() == SwtDialog.OK) {
@@ -139,35 +134,28 @@ public class Swt extends Common
 					System.err.println("Cannot load " + inputDialog.getValue());
 				}
 			}
-		}    
+		}
 	};
-  
-	protected Listener menuExitListener = new Listener()
-	{    
-		public void handleEvent(Event e)
-		{
-			Config.setWindow("main", new Rectangle(
-			    	shell.getLocation().x,
-			    	shell.getLocation().y,
-			    	shell.getSize().x,
-			    	shell.getSize().y));
+
+	protected Listener menuExitListener = new Listener() {
+		public void handleEvent(Event e) {
+			Config.setWindow("main", new Rectangle(shell.getLocation().x, shell.getLocation().y, shell.getSize().x,
+					shell.getSize().y));
 
 			System.exit(0);
-		}    
+		}
 	};
-  
-  
-	private Listener menuSelectDeviceListener = new Listener()
-	{    
-		public void handleEvent(Event e)
-		{
+
+	private Listener menuSelectDeviceListener = new Listener() {
+		public void handleEvent(Event e) {
 			if (selectDeviceDialog.open() == SwtDialog.OK) {
 				if (selectDeviceDialog.getSelectedDeviceEntry().equals(getDevice())) {
 					return;
 				}
 				if (MIDletBridge.getCurrentMIDlet() != getLauncher()) {
-					if (!SwtMessageDialog.openQuestion(shell,
-							"Question?", "Changing device needs MIDlet to be restarted. All MIDlet data will be lost. Are you sure?")) { 
+					if (!SwtMessageDialog
+							.openQuestion(shell, "Question?",
+									"Changing device needs MIDlet to be restarted. All MIDlet data will be lost. Are you sure?")) {
 						return;
 					}
 				}
@@ -183,130 +171,103 @@ public class Swt extends Common
 					startLauncher(MIDletBridge.getMIDletContext());
 				}
 			}
-		}    
+		}
 	};
-	
-	private StatusBarListener statusBarListener = new StatusBarListener()
-	{			
-		public void statusBarChanged(final String text) 
-		{			
-			shell.getDisplay().asyncExec(new Runnable()
-			{
-				public void run() 
-				{
+
+	private StatusBarListener statusBarListener = new StatusBarListener() {
+		public void statusBarChanged(final String text) {
+			shell.getDisplay().asyncExec(new Runnable() {
+				public void run() {
 					statusBar.setText(text);
 				}
 			});
-		}  
+		}
 	};
-  
-	private ResponseInterfaceListener responseInterfaceListener = new ResponseInterfaceListener()
-	{
-		public void stateChanged(final boolean state) 
-		{
-			shell.getDisplay().asyncExec(new Runnable()
-			{
-				public void run() 
-				{
+
+	private ResponseInterfaceListener responseInterfaceListener = new ResponseInterfaceListener() {
+		public void stateChanged(final boolean state) {
+			shell.getDisplay().asyncExec(new Runnable() {
+				public void run() {
 					menuOpenJADFile.setEnabled(state);
 					menuOpenJADURL.setEnabled(state);
 					menuSelectDevice.setEnabled(state);
 				}
 			});
-		}  
+		}
 	};
-  
-/*	WindowAdapter windowListener = new WindowAdapter()
-	{
-		public void windowClosing(WindowEvent ev) 
-		{
-			menuExitListener.actionPerformed(null);
-		}
-		
 
-		public void windowIconified(WindowEvent ev) 
-		{
-			MIDletBridge.getMIDletAccess(common.getLauncher().getCurrentMIDlet()).pauseApp();
-		}
-		
-		public void windowDeiconified(WindowEvent ev) 
-		{
-			try {
-				MIDletBridge.getMIDletAccess(common.getLauncher().getCurrentMIDlet()).startApp();
-			} catch (MIDletStateChangeException ex) {
-				System.err.println(ex);
-			}
-		}
-	};*/
-	
-	protected Swt(Shell shell)
-	{
+	/*
+	 * WindowAdapter windowListener = new WindowAdapter() { public void
+	 * windowClosing(WindowEvent ev) { menuExitListener.actionPerformed(null); }
+	 * 
+	 * 
+	 * public void windowIconified(WindowEvent ev) {
+	 * MIDletBridge.getMIDletAccess(common.getLauncher().getCurrentMIDlet()).pauseApp(); }
+	 * 
+	 * public void windowDeiconified(WindowEvent ev) { try {
+	 * MIDletBridge.getMIDletAccess(common.getLauncher().getCurrentMIDlet()).startApp(); }
+	 * catch (MIDletStateChangeException ex) { System.err.println(ex); } } };
+	 */
+
+	protected Swt(Shell shell) {
 		this(shell, null);
 	}
-  
-	protected Swt(Shell shell, DeviceEntry defaultDevice)
-	{
-		super(new EmulatorContext()
-		{
+
+	protected Swt(Shell shell, DeviceEntry defaultDevice) {
+		super(new EmulatorContext() {
 			private InputMethod inputMethod = new SwtInputMethod();
-			
+
 			private DeviceDisplay deviceDisplay = new SwtDeviceDisplay(this);
 
 			private FontManager fontManager = new SwtFontManager();
-			    
-			public DisplayComponent getDisplayComponent()
-			{
+
+			public DisplayComponent getDisplayComponent() {
 				return devicePanel.getDisplayComponent();
 			}
 
-            public InputMethod getDeviceInputMethod()
-            {
-                return inputMethod;
-            }    
+			public InputMethod getDeviceInputMethod() {
+				return inputMethod;
+			}
 
-            public DeviceDisplay getDeviceDisplay()
-            {
-                return deviceDisplay;
-            }
+			public DeviceDisplay getDeviceDisplay() {
+				return deviceDisplay;
+			}
 
-			public FontManager getDeviceFontManager() 
-			{
+			public FontManager getDeviceFontManager() {
 				return fontManager;
-			}    
+			}
 		});
 
 		initInterface(shell);
 
-//		addWindowListener(windowListener);
-		    
+		// addWindowListener(windowListener);
+
 		Config.loadConfig(null, emulatorContext);
-		
+
 		Rectangle window = Config.getWindow("main", new Rectangle(0, 0, 160, 120));
 		shell.setLocation(window.x, window.y);
-		
+
 		shell.addKeyListener(keyListener);
 
 		selectDeviceDialog = new SwtSelectDeviceDialog(shell, emulatorContext);
-    
+
 		setStatusBarListener(statusBarListener);
 		setResponseInterfaceListener(responseInterfaceListener);
-		
-	    Message.addListener(new SwtErrorMessageDialogPanel(shell));
+
+		Message.addListener(new SwtErrorMessageDialogPanel(shell));
 	}
-	
-	
-	protected void initInterface(Shell shell)
-	{
+
+	protected void initInterface(Shell shell) {
 		GridLayout layout = new GridLayout(1, false);
 		shell.setLayout(layout);
 		shell.setLayoutData(new GridData(GridData.FILL_BOTH));
 
 		Menu bar = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(bar);
-    
+
 		MenuItem menuFile = new MenuItem(bar, SWT.CASCADE);
 		menuFile.setText("File");
-    
+
 		Menu fileSubmenu = new Menu(shell, SWT.DROP_DOWN);
 		menuFile.setMenu(fileSubmenu);
 
@@ -319,14 +280,14 @@ public class Swt extends Common
 		menuOpenJADURL.addListener(SWT.Selection, menuOpenJADURLListener);
 
 		new MenuItem(fileSubmenu, SWT.SEPARATOR);
-    
+
 		MenuItem menuExit = new MenuItem(fileSubmenu, SWT.PUSH);
 		menuExit.setText("Exit");
 		menuExit.addListener(SWT.Selection, menuExitListener);
-    
+
 		MenuItem menuOptions = new MenuItem(bar, SWT.CASCADE);
 		menuOptions.setText("Options");
-    
+
 		Menu optionsSubmenu = new Menu(shell, SWT.DROP_DOWN);
 		menuOptions.setMenu(optionsSubmenu);
 
@@ -343,14 +304,12 @@ public class Swt extends Common
 		statusBar.setText("Status");
 		statusBar.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 	}
-      
-  
-	public void setDevice(DeviceEntry entry)
-	{
+
+	public void setDevice(DeviceEntry entry) {
 		if (DeviceFactory.getDevice() != null) {
-//			((SwtDevice) DeviceFactory.getDevice()).dispose();
+			// ((SwtDevice) DeviceFactory.getDevice()).dispose();
 		}
-		
+
 		try {
 			ClassLoader classLoader = getClass().getClassLoader();
 			if (entry.getFileName() != null) {
@@ -359,13 +318,11 @@ public class Swt extends Common
 				classLoader = Common.createExtensionsClassLoader(urls);
 			}
 
-			// TODO font manager have to be moved from emulatorContext into device
-			emulatorContext.getDeviceFontManager().init();		
-			
-			Device device = DeviceImpl.create(
-					emulatorContext, 
-					classLoader, 
-					entry.getDescriptorLocation());
+			// TODO font manager have to be moved from emulatorContext into
+			// device
+			emulatorContext.getDeviceFontManager().init();
+
+			Device device = DeviceImpl.create(emulatorContext, classLoader, entry.getDescriptorLocation());
 			this.deviceEntry = entry;
 			setDevice(device);
 			updateDevice();
@@ -375,38 +332,41 @@ public class Swt extends Common
 			System.err.println(ex);
 		}
 	}
-	
-	
-	protected void updateDevice()
-	{
+
+	protected void updateDevice() {
 		shell.setSize(shell.computeSize(SWT.DEFAULT, SWT.DEFAULT, true));
 	}
-  
-  
-	public static void main(String args[])
-	{
+
+	public static void main(String args[]) {
 		Display display = new Display();
 		shell = new Shell(display, SWT.CLOSE | SWT.TITLE | SWT.MIN);
-		    
+
 		List params = new ArrayList();
 		for (int i = 0; i < args.length; i++) {
 			params.add(args[i]);
 		}
 
 		Swt app = new Swt(shell);
+		app.initParams(params);
 		app.initDevice(params, app.selectDeviceDialog.getSelectedDeviceEntry());
 		app.updateDevice();
-		
+
 		shell.pack();
 		shell.open();
-		
-		app.initMIDlet(params, false);
-		
-		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ())
-				display.sleep ();
+
+		String midletString;
+		try {
+			midletString = (String) params.iterator().next();
+		} catch (NoSuchElementException ex) {
+			midletString = null;
 		}
-		display.dispose();			
+		app.initMIDlet(midletString, false);
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
 	}
 
 }
