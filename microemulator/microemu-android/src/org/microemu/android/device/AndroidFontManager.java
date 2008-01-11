2cfb26774db6ef603aef5e4c1360faa3bae11632
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2008-01-11 13:40:59 +0000

    Added license and copyright information
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1536 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-android/src/org/microemu/android/device/AndroidFontManager.java
+++ microemulator/microemu-android/src/org/microemu/android/device/AndroidFontManager.java
@@ -1,6 +1,6 @@
-/*
+/**
  *  MicroEmulator
- *  Copyright (C) 2001 Bartek Teodorczyk <barteo@barteo.net>
+ *  Copyright (C) 2008 Bartek Teodorczyk <barteo@barteo.net>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -15,8 +15,10 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  @version $Id$
  */
- 
+
 package org.microemu.android.device;
 
 import java.util.Hashtable;
