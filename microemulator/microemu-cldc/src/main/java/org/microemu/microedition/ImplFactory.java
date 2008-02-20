8c7895af58662d561ce95272b2f060dc124f5974
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2008-02-20 23:39:50 +0000

    register(Class delegate, Object implementationInstance)
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1592 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-cldc/src/main/java/org/microemu/microedition/ImplFactory.java
+++ microemulator/microemu-cldc/src/main/java/org/microemu/microedition/ImplFactory.java
@@ -33,82 +33,86 @@ import org.microemu.microedition.io.ConnectorDelegate;
  * This class allows to unbind implemenation with CLDC or MIDP declarations.
  * 
  * @author vlads
- *
+ * 
  */
 public class ImplFactory {
-	
+
 	public static final String DEFAULT = "org.microemu.default";
-	
-	private static final String INTERFACE_NAME_SUFIX = "Delegate";  
-	
+
+	private static final String INTERFACE_NAME_SUFIX = "Delegate";
+
 	private static final String IMPLEMENTATION_NAME_SUFIX = "Impl";
 
 	private Map implementations = new HashMap();
-	
+
 	private Map implementationsGCF = new HashMap();
 
 	/* The context to be used when loading classes and resources */
-    private AccessControlContext acc;
-    
-    /**
-     * Allow default initialization.
-     * In Secure environment instance() should be called initialy from secure contex.  
-     */
-    private static class SingletonHolder {
-        private static ImplFactory instance = new ImplFactory();
-    } 
-    
-    private ImplFactory() {
-    	acc = AccessController.getContext();
-    }
-    
-    public static ImplFactory instance() {
+	private AccessControlContext acc;
+
+	/**
+	 * Allow default initialization. In Secure environment instance() should be
+	 * called initialy from secure contex.
+	 */
+	private static class SingletonHolder {
+		private static ImplFactory instance = new ImplFactory();
+	}
+
+	private ImplFactory() {
+		acc = AccessController.getContext();
+	}
+
+	public static ImplFactory instance() {
 		return SingletonHolder.instance;
-    }
-    
-	public static void register(Class delegate, Class implementation) {
-		// TODO ImplementationUnloadable, verify classes
-		instance().implementations.put(delegate, implementation);
 	}
-	
+
+	public static void register(Class delegate, Class implementationClass) {
+		instance().implementations.put(delegate, implementationClass);
+	}
+
+	public static void register(Class delegate, Object implementationInstance) {
+		instance().implementations.put(delegate, implementationInstance);
+	}
+
 	public static void unregister(Class delegate, Class implementation) {
-		//TODO implement
+		// TODO implement
 	}
-	
+
 	/**
 	 * 
 	 * Register Generic Connection Framework scheme implementation.
 	 * 
-	 * @param implementation instance of ConnectorDelegate
+	 * @param implementation
+	 *            instance of ConnectorDelegate
 	 * @param scheme
 	 */
 	public static void registerGCF(String scheme, Object implementation) {
 		if (!ConnectorDelegate.class.isAssignableFrom(implementation.getClass())) {
-			throw new IllegalArgumentException(); 
+			throw new IllegalArgumentException();
 		}
 		if (scheme == null) {
 			scheme = DEFAULT;
-		} 
+		}
 		Object impl = instance().implementationsGCF.get(scheme);
 		if (impl instanceof ImplementationUnloadable) {
-			((ImplementationUnloadable)impl).unregisterImplementation();
+			((ImplementationUnloadable) impl).unregisterImplementation();
 		}
 		instance().implementationsGCF.put(scheme, implementation);
 	}
 
 	public static void unregistedGCF(String scheme, Object implementation) {
 		if (!ConnectorDelegate.class.isAssignableFrom(implementation.getClass())) {
-			throw new IllegalArgumentException(); 
+			throw new IllegalArgumentException();
 		}
 		if (scheme == null) {
 			scheme = DEFAULT;
-		} 
+		}
 		Object impl = instance().implementationsGCF.get(scheme);
 		if (impl == implementation) {
 			instance().implementationsGCF.remove(scheme);
 		}
 	}
-	
+
 	private Object getDefaultImplementation(Class delegateInterface) {
 		try {
 			String name = delegateInterface.getName();
@@ -138,77 +142,81 @@ public class ImplFactory {
 			throw new RuntimeException("Unable create " + implClass.getName() + " implementation", e);
 		}
 	}
-	
+
 	/**
 	 * 
-	 * @param name The URL for the connection.
+	 * @param name
+	 *            The URL for the connection.
 	 * @return UTL scheme
 	 */
 	public static String getCGFScheme(String name) {
 		return name.substring(0, name.indexOf(':'));
 	}
-	
+
 	/**
 	 * 
-	 * @param name The URL for the connection.
+	 * @param name
+	 *            The URL for the connection.
 	 * @return
 	 */
 	public static ConnectorDelegate getCGFImplementation(String name) {
 		String scheme = getCGFScheme(name);
-		ConnectorDelegate impl = (ConnectorDelegate)instance().implementationsGCF.get(scheme);
+		ConnectorDelegate impl = (ConnectorDelegate) instance().implementationsGCF.get(scheme);
 		if (impl != null) {
-			return impl;	
+			return impl;
 		}
-		impl = (ConnectorDelegate)instance().implementationsGCF.get(DEFAULT);
+		impl = (ConnectorDelegate) instance().implementationsGCF.get(DEFAULT);
 		if (impl != null) {
-			return impl;	
+			return impl;
 		}
-		return (ConnectorDelegate)instance().getDefaultImplementation(ConnectorDelegate.class);
+		return (ConnectorDelegate) instance().getDefaultImplementation(ConnectorDelegate.class);
 	}
-	
-//	public static Implementation getImplementation(Class origClass, Object[] constructorArgs) {
-//		//TO-DO constructorArgs
-//		return getImplementation(origClass);
-//	}
-	
+
+	// public static Implementation getImplementation(Class origClass, Object[]
+	// constructorArgs) {
+	// //TO-DO constructorArgs
+	// return getImplementation(origClass);
+	// }
+
 	public static Implementation getImplementation(Class origClass, Class delegateInterface) {
-		// if called from implementation constructor return null to avoid recurive calls!
-		//TODO can be done using thread stack analyse or ThreadLocal 
+		// if called from implementation constructor return null to avoid
+		// recurive calls!
+		// TODO can be done using thread stack analyse or ThreadLocal
 		Object impl = instance().implementations.get(delegateInterface);
-//		debugClassLoader(Implementation.class);
-//		debugClassLoader(origClass);
-//		debugClassLoader(delegateInterface);
-//		debugClassLoader(o);
-		
+		// debugClassLoader(Implementation.class);
+		// debugClassLoader(origClass);
+		// debugClassLoader(delegateInterface);
+		// debugClassLoader(o);
+
 		if (impl != null) {
 			if (impl instanceof Class) {
-				return (Implementation)instance().implementationNewInstance((Class)impl);
+				return (Implementation) instance().implementationNewInstance((Class) impl);
 			} else {
-				return (Implementation)impl;
+				return (Implementation) impl;
 			}
 		}
-		return (Implementation)instance().getDefaultImplementation(delegateInterface);
+		return (Implementation) instance().getDefaultImplementation(delegateInterface);
 	}
-	
-//	private static void debugClassLoader(Object obj) {
-//		if (obj == null) {
-//			System.out.print("no class");
-//			return;
-//		}
-//		Class klass;
-//		if (obj instanceof Class) {
-//			klass = (Class)obj;
-//			System.out.print("class ");
-//		} else {
-//			klass = obj.getClass();
-//			System.out.print("instance ");
-//		}
-//		System.out.print(klass.getName() + " loaded by ");
-//		if (klass.getClassLoader() != null) {
-//			System.out.print(klass.getClassLoader().hashCode());	
-//		} else {
-//			System.out.print("system");
-//		}
-//		System.out.println();
-//	}
+
+	// private static void debugClassLoader(Object obj) {
+	// if (obj == null) {
+	// System.out.print("no class");
+	// return;
+	// }
+	// Class klass;
+	// if (obj instanceof Class) {
+	// klass = (Class)obj;
+	// System.out.print("class ");
+	// } else {
+	// klass = obj.getClass();
+	// System.out.print("instance ");
+	// }
+	// System.out.print(klass.getName() + " loaded by ");
+	// if (klass.getClassLoader() != null) {
+	// System.out.print(klass.getClassLoader().hashCode());
+	// } else {
+	// System.out.print("system");
+	// }
+	// System.out.println();
+	// }
 }
