b25b0b65591f6e1a4642d4913cdea7cc636a425a
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2008-02-25 21:07:14 +0000

    Added Apache License Version 2.0
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1605 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase-swt/src/main/java/com/barteo/emulator/device/swt/SwtDevice.java
+++ microemulator/microemu-javase-swt/src/main/java/com/barteo/emulator/device/swt/SwtDevice.java
@@ -2,19 +2,24 @@
  *  MicroEmulator
  *  Copyright (C) 2002-2006 Bartek Teodorczyk <barteo@barteo.net>
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
+ *  It is licensed under the following two licenses as alternatives:
+ *    1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
+ *    2. Apache License (the "AL") Version 2.0
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *  You may not use this file except in compliance with at least one of
+ *  the above two licenses.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You may obtain a copy of the LGPL at
+ *      http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ *
+ *  You may obtain a copy of the AL at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the LGPL or the AL for the specific language governing permissions and
+ *  limitations.
  */
 
 package com.barteo.emulator.device.swt;
