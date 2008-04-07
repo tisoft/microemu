6eae27193f1e6e7653d64c6ef52703b3badc318b
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2008-04-07 15:42:11 +0000

    fix: cancel unscheduled Timer
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1677 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase/src/main/java/org/microemu/app/util/MIDletTimer.java
+++ microemulator/microemu-javase/src/main/java/org/microemu/app/util/MIDletTimer.java
@@ -39,78 +39,79 @@ import org.microemu.MIDletContext;
 import org.microemu.log.Logger;
 
 /**
- * Terminate all timers on MIDlet exit.
- * TODO Name all the timer Threads created by MIDlet in Java 5  
+ * Terminate all timers on MIDlet exit. TODO Name all the timer Threads created
+ * by MIDlet in Java 5
+ * 
  * @author vlads
  */
 public class MIDletTimer extends Timer {
 
 	private static Map midlets = new WeakHashMap();
-	
+
 	private class OneTimeTimerTaskWrapper extends TimerTask {
 
 		TimerTask task;
-		
+
 		OneTimeTimerTaskWrapper(TimerTask task) {
 			this.task = task;
 		}
-		
+
 		public void run() {
 			unregister(MIDletTimer.this);
 			task.run();
 		}
-		
+
 	}
-	
+
 	private String name;
-	
+
 	private MIDletContext midletContext;
-	
+
 	public MIDletTimer() {
 		super();
 		StackTraceElement[] ste = new Throwable().getStackTrace();
-		name = ste[1].getClassName() + "." + ste[1].getMethodName(); 
+		name = ste[1].getClassName() + "." + ste[1].getMethodName();
 	}
-	
+
 	public void schedule(TimerTask task, Date time) {
 		register(this);
 		super.schedule(new OneTimeTimerTaskWrapper(task), time);
 	}
-	
+
 	public void schedule(TimerTask task, Date firstTime, long period) {
 		register(this);
 		super.schedule(task, firstTime, period);
 	}
-	
+
 	public void schedule(TimerTask task, long delay) {
 		register(this);
 		super.schedule(new OneTimeTimerTaskWrapper(task), delay);
 	}
-	
+
 	public void schedule(TimerTask task, long delay, long period) {
 		register(this);
 		super.schedule(task, delay, period);
 	}
-	
+
 	public void scheduleAtFixedRate(TimerTask task, Date firstTime, long period) {
 		register(this);
 		super.schedule(task, firstTime, period);
 	}
-	
+
 	public void scheduleAtFixedRate(TimerTask task, long delay, long period) {
 		register(this);
 		super.scheduleAtFixedRate(task, delay, period);
 	}
-	
+
 	public void cancel() {
-    	unregister(this);
+		unregister(this);
 		super.cancel();
 	}
 
 	private void terminate() {
 		super.cancel();
 	}
-	
+
 	private static void register(MIDletTimer timer) {
 		if (timer.midletContext == null) {
 			timer.midletContext = MIDletBridge.getMIDletContext();
@@ -119,43 +120,43 @@ public class MIDletTimer extends Timer {
 			Logger.error("Creating Timer with no MIDlet context", new Throwable());
 			return;
 		}
-		Map timers = (Map)midlets.get(timer.midletContext);
+		Map timers = (Map) midlets.get(timer.midletContext);
 		if (timers == null) {
 			// Can't use WeakHashMap Timers are disposed by JVM
 			timers = new HashMap();
 			midlets.put(timer.midletContext, timers);
 		}
-		//Logger.debug("Register timer created from [" + timer.name + "]");
+		// Logger.debug("Register timer created from [" + timer.name + "]");
 		timers.put(timer, timer.midletContext);
 	}
-	
+
 	private static void unregister(MIDletTimer timer) {
 		if (timer.midletContext == null) {
-			Logger.error("Timer with no MIDlet context", new Throwable());
+			// Logger.error("Timer with no MIDlet context", new Throwable());
 			return;
 		}
-		Map timers = (Map)midlets.get(timer.midletContext);
+		Map timers = (Map) midlets.get(timer.midletContext);
 		if (timers == null) {
 			return;
 		}
-		//Logger.debug("Unregister timer created from [" + timer.name + "]");
+		// Logger.debug("Unregister timer created from [" + timer.name + "]");
 		timers.remove(timer);
 	}
-	
+
 	/**
-	 * Termnate all Threads created by MIDlet
+	 * Terminate all Threads created by MIDlet
 	 */
 	public static void contextDestroyed(MIDletContext midletContext) {
 		if (midletContext == null) {
 			return;
 		}
-		Map timers = (Map)midlets.get(midletContext);
+		Map timers = (Map) midlets.get(midletContext);
 		if (timers != null) {
 			terminateTimers(timers);
 			midlets.remove(midletContext);
 		}
 	}
-	
+
 	private static void terminateTimers(Map timers) {
 		for (Iterator iter = timers.keySet().iterator(); iter.hasNext();) {
 			Object o = iter.next();
@@ -163,13 +164,14 @@ public class MIDletTimer extends Timer {
 				continue;
 			}
 			if (o instanceof MIDletTimer) {
-				MIDletTimer tm = (MIDletTimer)o;
+				MIDletTimer tm = (MIDletTimer) o;
 				Logger.warn("MIDlet timer created from [" + tm.name + "] still running");
 				tm.terminate();
 			} else {
 				Logger.debug("unrecognized Object [" + o.getClass().getName() + "]");
 			}
-		};
+		}
+		;
 	}
 
 }
