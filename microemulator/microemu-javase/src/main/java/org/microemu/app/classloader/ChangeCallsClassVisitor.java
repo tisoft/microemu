7356b59a16af1baef9a4d7eb75b267701420c8ca
vlads <vlads@272dc960-a678-11de-8dbb-b5444cbab91d>
2008-06-04 05:59:27 +0000

    Logger.warn("Loading MIDlet class " + name + " of version 1.5 or 1.6
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1735 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase/src/main/java/org/microemu/app/classloader/ChangeCallsClassVisitor.java
+++ microemulator/microemu-javase/src/main/java/org/microemu/app/classloader/ChangeCallsClassVisitor.java
@@ -26,33 +26,54 @@
  */
 package org.microemu.app.classloader;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.microemu.app.util.MIDletThread;
+import org.microemu.log.Logger;
 import org.objectweb.asm.ClassAdapter;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 
 /**
  * @author vlads
- *
+ * 
  */
 public class ChangeCallsClassVisitor extends ClassAdapter {
 
 	InstrumentationConfig config;
-	
+
+	static final Map javaVersion = new HashMap();
+
+	static {
+		javaVersion.put(new Integer(0x3002D), "1.1");
+		javaVersion.put(new Integer(0x3002E), "1.2");
+		javaVersion.put(new Integer(47), "1.3");
+		javaVersion.put(new Integer(48), "1.4");
+		javaVersion.put(new Integer(49), "1.5");
+		javaVersion.put(new Integer(50), "1.6");
+	}
+
 	public ChangeCallsClassVisitor(ClassVisitor cv, InstrumentationConfig config) {
 		super(cv);
 		this.config = config;
 	}
 
-    public void visit(final int version, final int access, final String name, final String signature, String superName, final String[] interfaces) {
-    	if  ((config.isEnhanceThreadCreation()) && (superName.equals("java/lang/Thread"))) {
-    		superName = ChangeCallsMethodVisitor.codeName(MIDletThread.class);
-    	}
-    	super.visit(version, access, name, signature, superName, interfaces);
+	public void visit(final int version, final int access, final String name, final String signature, String superName,
+			final String[] interfaces) {
+		if ((0xFF & version) >= 49) {
+			String v = (String) javaVersion.get(new Integer(version));
+			Logger.warn("Loading MIDlet class " + name + " of version " + version + ((v == null) ? "" : (" " + v)));
+		}
+		if ((config.isEnhanceThreadCreation()) && (superName.equals("java/lang/Thread"))) {
+			superName = ChangeCallsMethodVisitor.codeName(MIDletThread.class);
+		}
+		super.visit(version, access, name, signature, superName, interfaces);
 	}
-    
-	public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
-		return  new ChangeCallsMethodVisitor(super.visitMethod(access, name, desc, signature, exceptions), config);
+
+	public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature,
+			final String[] exceptions) {
+		return new ChangeCallsMethodVisitor(super.visitMethod(access, name, desc, signature, exceptions), config);
 	}
 
 }
