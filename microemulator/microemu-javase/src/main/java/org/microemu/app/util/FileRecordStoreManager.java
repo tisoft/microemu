4c238631bbe9e27296eadc91919d5f44a7c5d515
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2009-04-03 07:20:31 +0000

    Added buffering for stream (not sure though of any performance improvement)
    git-svn-id: http://microemu.googlecode.com/svn/trunk@1986 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase/src/main/java/org/microemu/app/util/FileRecordStoreManager.java
+++ microemulator/microemu-javase/src/main/java/org/microemu/app/util/FileRecordStoreManager.java
@@ -24,6 +24,8 @@
 
 package org.microemu.app.util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -250,7 +252,7 @@ public class FileRecordStoreManager implements RecordStoreManager {
 	private RecordStoreImpl loadFromDiskSecure(File recordStoreFile) throws FileNotFoundException {
 		RecordStoreImpl store = null;
 		try {
-			DataInputStream dis = new DataInputStream(new FileInputStream(recordStoreFile));
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(recordStoreFile)));
 			store = new RecordStoreImpl(this, dis);
 			dis.close();
 		} catch (FileNotFoundException e) {
@@ -286,7 +288,7 @@ public class FileRecordStoreManager implements RecordStoreManager {
 			}
 		}
 		try {
-			DataOutputStream dos = new DataOutputStream(new FileOutputStream(recordStoreFile));
+			DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(recordStoreFile)));
 			recordStore.write(dos);
 			dos.close();
 		} catch (IOException e) {
