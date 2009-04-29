6d32b99f1829f4bdc5a6348ad8f0fd105e5fd90b
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2009-04-29 18:22:42 +0000

    Added license header
    git-svn-id: http://microemu.googlecode.com/svn/trunk@2031 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-android/src/org/microemu/android/device/ui/AndroidCustomItemUI.java
+++ microemulator/microemu-android/src/org/microemu/android/device/ui/AndroidCustomItemUI.java
@@ -1,3 +1,29 @@
+/**
+ *  MicroEmulator
+ *  Copyright (C) 2009 Bartek Teodorczyk <barteo@barteo.net>
+ *
+ *  It is licensed under the following two licenses as alternatives:
+ *    1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
+ *    2. Apache License (the "AL") Version 2.0
+ *
+ *  You may not use this file except in compliance with at least one of
+ *  the above two licenses.
+ *
+ *  You may obtain a copy of the LGPL at
+ *      http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ *
+ *  You may obtain a copy of the AL at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the LGPL or the AL for the specific language governing permissions and
+ *  limitations.
+ *
+ *  @version $Id: AndroidCustomItemUI.java 1918 2009-01-21 12:56:43Z barteo $
+ */
+
 package org.microemu.android.device.ui;
 
 import javax.microedition.lcdui.Graphics;
