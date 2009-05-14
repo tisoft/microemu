3cc3d415d5d9e16f4083bdfc6d01fc63b21f682e
barteo <barteo@272dc960-a678-11de-8dbb-b5444cbab91d>
2009-05-14 14:16:52 +0000

    [Android] Option to intercept exception messages
    git-svn-id: http://microemu.googlecode.com/svn/trunk@2057 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-android/src/org/microemu/android/asm/AndroidClassVisitor.java
+++ microemulator/microemu-android/src/org/microemu/android/asm/AndroidClassVisitor.java
@@ -26,15 +26,37 @@
 
 package org.microemu.android.asm;
 
+import java.util.HashMap;
+
+import org.microemu.Injected;
 import org.objectweb.asm.ClassAdapter;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodAdapter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 public class AndroidClassVisitor extends ClassAdapter {
 	
+	private static final String INJECTED_CLASS = codeName(Injected.class);
+
+	private static boolean enhanceCatchBlock = false;
+	
 	boolean isMidlet;
+	
+	private HashMap<Label,CatchInformation> catchInfo;
+	
+	private static class CatchInformation {
+		
+		Label label; 
+		
+		String type;
+
+		public CatchInformation(String type) {
+			this.label = new Label();
+			this.type = type;
+		}
+	}
 	
 	public class AndroidMethodVisitor extends MethodAdapter {
 
@@ -53,12 +75,47 @@ public class AndroidClassVisitor extends ClassAdapter {
 			mv.visitMethodInsn(opcode, owner, name, desc);
 		}
 		
+	    public void visitTryCatchBlock(final Label start, final Label end, final Label handler, final String type) {
+	    	if (enhanceCatchBlock && type != null) {
+	    		if (catchInfo == null) {
+	    			catchInfo = new HashMap<Label,CatchInformation>(); 
+	    		}
+	    		CatchInformation newHandler = (CatchInformation)catchInfo.get(handler);
+	    		if (newHandler == null) {
+	    			newHandler = new CatchInformation(type);
+	    			catchInfo.put(handler, newHandler);
+	    		}
+	    		mv.visitTryCatchBlock(start, end, newHandler.label, type);
+	    	} else {
+	    		mv.visitTryCatchBlock(start, end, handler, type);
+	    	}
+		}
+	    
+	    //TODO make this work for gMaps case
+	    public void visitLabel(Label label) {
+	    	if (enhanceCatchBlock && catchInfo != null) {
+	    		CatchInformation newHandler = (CatchInformation)catchInfo.get(label);
+	    		if (newHandler != null) {
+	    			mv.visitLabel(newHandler.label);
+	    			// no push, just use current Throwable in stack
+	    			mv.visitMethodInsn(Opcodes.INVOKESTATIC, INJECTED_CLASS, "handleCatchThrowable", "(Ljava/lang/Throwable;)Ljava/lang/Throwable;");
+	    			// stack contains Throwable, just verify that it is right type for this handler
+	        		mv.visitTypeInsn(Opcodes.CHECKCAST, newHandler.type);
+	    		}	
+	    	}
+	    	mv.visitLabel(label);
+	    }
+		
 	}
 
 	public AndroidClassVisitor(ClassVisitor cv, boolean isMidlet) {
 		super(cv);
 		
 		this.isMidlet = isMidlet;
+	}
+
+	public static String codeName(Class<Injected> klass) {
+		return klass.getName().replace('.', '/');
 	}
 
 	public MethodVisitor visitMethod(final int access, final String name, String desc, final String signature, final String[] exceptions) {
