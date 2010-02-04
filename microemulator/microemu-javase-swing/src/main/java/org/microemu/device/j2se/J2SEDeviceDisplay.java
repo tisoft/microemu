ab194f68fd7eed8977bcb88c6bce216d578ef9bd
barteo@gmail.com <barteo@gmail.com@272dc960-a678-11de-8dbb-b5444cbab91d>
2010-02-04 18:20:10 +0000

    Prevent NPE on early GameCanvas.flushGraphics() calls
    git-svn-id: http://microemu.googlecode.com/svn/trunk@2309 272dc960-a678-11de-8dbb-b5444cbab91d

--- microemulator/microemu-javase-swing/src/main/java/org/microemu/device/j2se/J2SEDeviceDisplay.java
+++ microemulator/microemu-javase-swing/src/main/java/org/microemu/device/j2se/J2SEDeviceDisplay.java
@@ -440,13 +440,15 @@ public class J2SEDeviceDisplay implements DeviceDisplayImpl
     public void flushGraphics(int x, int y, int width, int height) {
         if (gameCanvasImage != null) {
             J2SEGraphicsSurface surface = ((SwingDisplayComponent) context.getDisplayComponent()).getGraphicsSurface();
-            surface.getGraphics().drawImage(
-                    ((J2SEMutableImage) gameCanvasImage).getImage(), 
-                    x, y, x + width, y + height, 
-                    x, y, x + width, y + height, 
-                    null);
-            ((SwingDisplayComponent) context.getDisplayComponent()).fireDisplayRepaint(
-                    surface, 0, 0, surface.getImage().getWidth(), surface.getImage().getHeight());
+            if (surface != null) {
+                surface.getGraphics().drawImage(
+                        ((J2SEMutableImage) gameCanvasImage).getImage(), 
+                        x, y, x + width, y + height, 
+                        x, y, x + width, y + height, 
+                        null);
+                ((SwingDisplayComponent) context.getDisplayComponent()).fireDisplayRepaint(
+                        surface, 0, 0, surface.getImage().getWidth(), surface.getImage().getHeight());
+            }
         }
     }
 
