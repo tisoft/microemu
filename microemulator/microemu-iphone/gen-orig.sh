73ffc14613c9792d132684b1d7536c0841d29ef2
Markus Heberling <markus@tisoft.de>
2010-06-21 22:41:08 +0200

    changed path

--- microemulator/microemu-iphone/gen-orig.sh
+++ microemulator/microemu-iphone/gen-orig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mvn clean package -DANDROID_SDK=/Applications/Development/Android/android-sdk-mac_x86-1.6_r1/platforms/android-1.5
+mvn clean package -DANDROID_SDK=/Users/markus/Downloads/android-sdk-mac_86/platforms/android-7
 mkdir target/extracted
 unzip target/microemu-iphone-3.0.0-SNAPSHOT.jar -d target/extracted
-java -jar ../../../../xmlvm/dist/xmlvm.jar --target=iphone --out=target --iphone-app=MicroEmu --in=target/extracted/
+java -jar ../../../../xmlvm/dist/xmlvm.jar --target=iphone --out=target --iphone-app=MicroEmu --in=target/extracted/ --resource=target/extracted/
 #cd target
 #make
