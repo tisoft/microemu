2b3f85c65be997374f9535dce4416d7ae5a99de6
Markus Heberling <markus@tisoft.de>
2010-06-21 22:25:05 +0200

    added Canvas implementation

---
+++ microemulator/microemu-iphone/src/main/java/org/microemu/iphone/device/IPhoneMutableImage.java
@@ -0,0 +1,120 @@
+/**
+ *  MicroEmulator
+ *  Copyright (C) 2008 Bartek Teodorczyk <barteo@barteo.net>
+ *  Copyright (C) 2008 Markus Heberling <markus@heberling.net>
+ *
+ *  It is licensed under the following two licenses as alternatives:
+ *    1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
+ *    2. Apache License (the "AL") Version 2.0
+ *
+ *  You may not use this file except in compliance with at least one of
+ *  the above two licenses.
+ *
+ *  You may obtain a copy of the LGPL at
+ *      http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ *
+ *  You may obtain a copy of the AL at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the LGPL or the AL for the specific language governing permissions and
+ *  limitations.
+ *
+ *  @version $Id$
+ */
+package org.microemu.iphone.device;
+
+import javax.microedition.lcdui.Graphics;
+
+import org.microemu.device.MutableImage;
+
+import org.xmlvm.iphone.CGBitmapContext;
+import org.xmlvm.iphone.CGContext;
+import org.xmlvm.iphone.CGImage;
+
+public class IPhoneMutableImage extends MutableImage implements IPhoneImage {
+
+	private CGContext imageContext;
+	private int width;
+	private int height;
+	private IPhoneDisplayGraphics displayGraphics;
+
+	public IPhoneMutableImage(int width, int height) {
+		this.width = width;
+		this.height = height;
+		imageContext = CGBitmapContext.createWithSize(width, height);
+		displayGraphics = new IPhoneDisplayGraphics(imageContext, getWidth(), getHeight(), true);
+	}
+
+	@Override
+	public int[] getData() {
+		throw new UnsupportedOperationException("Currently not supported on iPhone");
+	}
+
+	@Override
+	public Graphics getGraphics() {
+		return displayGraphics;
+	}
+
+	@Override
+	public boolean isMutable() {
+		return true;
+	}
+
+	public CGImage getBitmap() {
+		displayGraphics.flushRenderQueue();
+        CGContext.UIGraphicsPushContext(imageContext);
+		try{
+            return CGBitmapContext.UIGraphicsGetImageFromCurrentImageContext().getCGImage();
+        } finally {
+            CGContext.UIGraphicsPopContext();
+        }
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public void getRGB(int[] argb, int offset, int scanlength, int x, int y, int width, int height) {
+
+		if (width <= 0 || height <= 0)
+			return;
+		if (x < 0 || y < 0 || x + width > getWidth() || y + height > getHeight())
+			throw new IllegalArgumentException("Specified area exceeds bounds of image");
+		if ((scanlength < 0 ? -scanlength : scanlength) < width)
+			throw new IllegalArgumentException("abs value of scanlength is less than width");
+		if (argb == null)
+			throw new NullPointerException("null rgbData");
+		if (offset < 0 || offset + width > argb.length)
+			throw new ArrayIndexOutOfBoundsException();
+		if (scanlength < 0) {
+			if (offset + scanlength * (height - 1) < 0)
+				throw new ArrayIndexOutOfBoundsException();
+		} else {
+			if (offset + scanlength * (height - 1) + width > argb.length)
+				throw new ArrayIndexOutOfBoundsException();
+		}
+
+//		throw new UnsupportedOperationException("Currently not supported on iPhone");
+
+		// bitmap.getPixels(argb, offset, scanlength, x, y, width, height);
+
+		/*
+		 * for (int i = 0; i < argb.length; i++) { int a = (argb[i] &
+		 * 0xFF000000); int b = (argb[i] & 0x00FF0000) >>> 16; int g = (argb[i]
+		 * & 0x0000FF00) >>> 8; int r = (argb[i] & 0x000000FF);
+		 * 
+		 * argb[i] = a | (r << 16) | (g << 8) | b; }
+		 */
+	}
+
+}
--- microemulator/microemu-iphone/src/main/java/org/microemu/iphone/device/ui/IPhoneCanvasUI.java
+++ microemulator/microemu-iphone/src/main/java/org/microemu/iphone/device/ui/IPhoneCanvasUI.java
@@ -1,24 +1,140 @@
+/**
+ *  MicroEmulator
+ *  Copyright (C) 2008 Markus Heberling <markus@heberling.net>
+ *
+ *  It is licensed under the following two licenses as alternatives:
+ *    1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
+ *    2. Apache License (the "AL") Version 2.0
+ *
+ *  You may not use this file except in compliance with at least one of
+ *  the above two licenses.
+ *
+ *  You may obtain a copy of the LGPL at
+ *      http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ *
+ *  You may obtain a copy of the AL at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the LGPL or the AL for the specific language governing permissions and
+ *  limitations.
+ *
+ *  @version $Id$
+ */
 package org.microemu.iphone.device.ui;
 
-import org.microemu.device.ui.CanvasUI;
-import org.microemu.iphone.MicroEmulator;
+import java.awt.event.KeyEvent;
 
 import javax.microedition.lcdui.Canvas;
 
-public class IPhoneCanvasUI extends AbstractDisplayableUI<Canvas> implements CanvasUI{
-    public IPhoneCanvasUI(MicroEmulator microEmulator, Canvas displayable) {
-        super(microEmulator, displayable);
-    }
+import org.microemu.MIDletAccess;
+import org.microemu.MIDletBridge;
+import org.microemu.device.ui.CanvasUI;
+import org.microemu.iphone.MicroEmulator;
+import org.microemu.iphone.device.IPhoneMutableImage;
+import org.xmlvm.iphone.CGContext;
+import org.xmlvm.iphone.CGImage;
+import org.xmlvm.iphone.CGPoint;
+import org.xmlvm.iphone.CGRect;
+import org.xmlvm.iphone.UIColor;
+import org.xmlvm.iphone.UITextField;
+import org.xmlvm.iphone.UIToolbar;
+import org.xmlvm.iphone.UITouch;
+import org.xmlvm.iphone.UIView;
 
-    public void hideNotify() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+public class IPhoneCanvasUI extends AbstractDisplayableUI<Canvas> implements CanvasUI {
 
-    public void showNotify() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+	enum Touch {
+		BEGIN, END, DRAG
+	}
 
-    public void invalidate() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+	private final class CanvasView extends UIView {
+		private final Canvas canvas;
+		private UITextField keyboardHandler;
+		private String oldText = "";
+		private boolean keybordVisible=false;
+		private IPhoneMutableImage offscreen;
+
+		private CanvasView(CGRect bounds, Canvas canvas) {
+            super(bounds);
+			this.canvas = canvas;
+			//setMultipleTouchEnabled(true);
+			setUserInteractionEnabled(true);
+			setClearsContextBeforeDrawing(false);
+//			setClearsContext$(Static.NO);
+			setBackgroundColor(UIColor.clearColor);
+
+			offscreen=new IPhoneMutableImage(canvas.getWidth(), canvas.getHeight());
+		}
+
+
+		@Override
+		public void drawRect(CGRect arg0) {
+			System.out.println("drawRect: " + canvas + " " + this);
+			MIDletAccess ma = MIDletBridge.getMIDletAccess();
+			if (ma == null) {
+				return;
+			}
+			CGContext context = CGContext.UICurrentContext();
+            context.translate(0, canvas.getHeight());
+            context.scale(1.0f, -1.0f);
+
+            context.setFillColor(new float[]{0.5f,1,1,0.5f});
+            context.fillRect(new CGRect(10,10,20,20));
+
+//			CoreGraphics.CGContextSaveGState(context);
+//			Graphics g = new IPhoneDisplayGraphics(context, canvas.getWidth(), canvas.getHeight(), false);
+			ma.getDisplayAccess().paint(offscreen.getGraphics());
+
+			CGRect rect=new CGRect(0, 0, offscreen.getWidth(), offscreen.getHeight());
+	        CGImage bitmap = offscreen.getBitmap();
+            context.drawImage(rect, bitmap);
+//			CoreGraphics.CGImageRelease(bitmap);
+//			g.drawString("XXX", 100, 100, 0);
+//			CoreGraphics.CGContextRestoreGState(context);
+		}
+	}
+
+	private UIView canvasView;
+
+	private UIView view;
+
+	public IPhoneCanvasUI(MicroEmulator microEmulator, final Canvas canvas) {
+		super(microEmulator, canvas);
+	}
+
+	public void hideNotify() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void invalidate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void showNotify() {
+		if (view == null) {
+			canvasView = new CanvasView(microEmulator.getWindow().getBounds(), displayable);
+
+			view = new UIView(microEmulator.getWindow().getBounds());
+			// tableView = new UITableView().initWithFrame$style$(
+			// new CGRect(0, 0, microEmulator.getWindow().bounds().size.width,
+			// microEmulator.getWindow().bounds().size.height - 40), 0);
+			view.addSubview(canvasView);
+			toolbar = (UIToolbar) new UIToolbar(new CGRect(0,
+					microEmulator.getWindow().getBounds().size.height - TOOLBAR_HEIGHT,
+					microEmulator.getWindow().getBounds().size.width, TOOLBAR_HEIGHT));
+			view.addSubview(toolbar);
+			updateToolbar();
+		}
+		microEmulator.getWindow().addSubview(view);
+	}
+
+	public UIView getCanvasView() {
+		return canvasView;
+	}
+
 }
